c375d7f8d205d42021cd53b46d8d28f7ffc22896
AsemDiab <asemhesham55@gmail.com>
2026-03-30 10:56:06 +0200

    fix the .env problem

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config();
+require("dotenv").config({ path: __dirname + "/.env" });
 async function connectDB() {
   const result = await mongoose.connect(process.env.MONGO_URI);
   console.log(process.env.MONGO_URI);
